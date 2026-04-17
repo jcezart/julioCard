6bddf89cf332555bff1071858732e50176655aed
Julio Cezar <jcezar.t@gmail.com>
2026-04-17 08:30:32 -0300

    card personalization

--- JulioCard/ContentView.swift
+++ JulioCard/ContentView.swift
@@ -10,12 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-                Color(red: 44/255, green: 62/255, blue: 80/255)
-                    .ignoresSafeArea()
-            Text("Julio Cezar")
-                .font(.title)
-                .bold()
-                .foregroundColor(.white)
+            Color(red: 44/255, green: 62/255, blue: 80/255)
+                .ignoresSafeArea()
+            VStack {
+                Image("julio")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 140.0, height: 180.0)
+                    .clipShape(.circle)
+                    .overlay(
+                        Circle().stroke(Color.gray, lineWidth: 5)
+                    )
+                Text("Julio Cezar")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS and Android Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(.white)
+                        .frame(height: 50)
+                    HStack{
+                        Image(systemName: "phone.circle")
+                            .foregroundStyle(Color.green)
+                        Text("+55 41 992330843")
+                        
+                    }
+                }
+                .padding()
+                
+            }
         }
 
     }
